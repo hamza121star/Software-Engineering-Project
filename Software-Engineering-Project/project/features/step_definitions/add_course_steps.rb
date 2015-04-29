5f51be9b8a189355208c3ff474c5308f0112a050
Hamza Mahmood <hamza.mahmood1993@gmail.com>
2015-04-29 14:33:25 -0300

    added rspec tests

---
+++ Software-Engineering-Project/project/features/step_definitions/add_course_steps.rb
@@ -0,0 +1,10 @@
+# Add a declarative step here for populating the DB with movies.
+
+#Given /logged as an instructor/ do |courses_table|
+ # courses_table.hashes.each do |course|
+ #   Course.create!(course)
+ # end
+#end
+
+# See the input course
+#Then /I should see "(.*)"/
---
+++ Software-Engineering-Project/project/features/step_definitions/signup.rb
@@ -0,0 +1,16 @@
+# Helper methods
+
+#def create_guest
+#  @guest = @guest || {name: "xxx", email: "xxx@gmail.com", password: "xxx",
+#                    password_confirm: "xxx"}
+#end
+
+#def find_user
+#  @user = @user || User.first
+#  conditions:{:email => @guest[:email]}
+#end
+
+#def create_user
+#  create_guest
+#  delete_user
+#end
